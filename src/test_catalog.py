d7074017babe74e2e232d25bff4524650c34d343
Yogithaa S <yogithaa2006@gmail.com>
2026-09-22 20:39:19 +0530

    Rename glucose test in bugfix

--- src/test_catalog.py
+++ src/test_catalog.py
@@ -1,7 +1,7 @@
 def get_available_tests():
     return [
         "Complete Blood Count (CBC)",
-        "Blood Glucose Test",
+        "Diabetes Blood Glucose Test",
         "Lipid Profile",
         "Thyroid Function Test"
     ]
